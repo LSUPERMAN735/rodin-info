9afa905bd4bc8c825fc9dd3b7994817cc739c9be
LSUPERMAN735 <lsuperman735@outlook.fr>
2016-09-01 14:52:11 +0200

    Add files via upload

---
+++ RODINInfo.W10/Layouts/List/ListGrouped.xaml.cs
@@ -0,0 +1,15 @@
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using AppStudio.Uwp.Controls;
+using RODINInfo.ViewModels;
+
+namespace RODINInfo.Layouts.List
+{
+    public sealed partial class ListGrouped : ListLayoutBase
+    {
+        public ListGrouped()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
