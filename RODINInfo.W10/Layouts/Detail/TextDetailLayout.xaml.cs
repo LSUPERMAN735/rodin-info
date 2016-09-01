00fbf921f6dbc05251f9177fb3df5d3d2048330a
LSUPERMAN735 <lsuperman735@outlook.fr>
2016-09-01 14:50:10 +0200

    Add files via upload

---
+++ RODINInfo.W10/Layouts/Detail/TextDetailLayout.xaml.cs
@@ -0,0 +1,10 @@
+namespace RODINInfo.Layouts.Detail
+{
+    public sealed partial class TextDetailLayout : BaseDetailLayout
+    {
+        public TextDetailLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
